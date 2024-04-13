4d906a21285092b6cd22fae45a342ae2899c82bf
TashdidDiganta <tashdiddiganta@gmail.com>
2024-04-13 23:06:56 +0600

    added file

--- wp-content/themes/herobiz/functions.php
+++ wp-content/themes/herobiz/functions.php
@@ -13,6 +13,37 @@ define('HEROBIZ_THEME_INC', HEROBIZ_THEME_DIR . '/inc/' );
 
 
 
+
+  function themeslug_customize_register( $wp_customize ) {
+    // Do stuff with $wp_customize, the WP_Customize_Manager object.
+
+    $wp_customize->add_panel( 'herobiz_settings', array(
+        'title' => __( 'Herobiz Settings' ),
+        'description' => 'Herobiz Header', // Include html tags such as <p>.
+        'priority' => 40, // Mixed with top-level-section hierarchy.
+      ) );
+      $wp_customize->add_section( 'herobiz_header' , array(
+        'title' => __( 'Herobiz Header Settings' ),
+        'panel' => 'herobiz_settings',
+      ) );
+
+      $wp_customize->add_setting( 'header_settings', array(
+        'default' => 'Header Item',
+        'transport' => 'refresh',
+      ) );
+
+      $wp_customize->add_control( 'custom_theme_css', array(
+        'label' => __( 'Custom Theme CSS' ),
+        'type' => 'textarea',
+        'section' => 'herobiz_header',
+        'settings' => 'header_settings',
+      ) );
+  }
+
+
+  add_action( 'customize_register', 'themeslug_customize_register');
+
+
 require HEROBIZ_THEME_INC . 'theme_helper.php';
 
 /**
